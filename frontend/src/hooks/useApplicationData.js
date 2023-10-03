5ecd906cb79fd7e61c8276f0fb9d2e3a6b3800e3
Jonas Cura <jonasm.cura@gmail.com.com>
2023-10-03 16:38:26 -0600

    update useState to useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,71 +1,64 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 
+const initialState = {
+  photos,
+  selectedPhoto: null,
+  favoritePhotos: [],
+  topics,
+  modal:false
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SELECT_PHOTO':
+      return {
+        ...state,
+        selectedPhoto: action.payload,
+        modal: true
+      };
+    case 'TOGGLE_FAVORITE':
+      const isPhotoInFavorites = state.favoritePhotos.includes(action.payload);
+      return {
+        ...state,
+        favoritePhotos: isPhotoInFavorites
+          ? state.favoritePhotos.filter(id => id !== action.payload)
+          : [...state.favoritePhotos, action.payload]
+      };
+    case 'CLOSE_MODAL':
+      return {
+        ...state,
+        selectedPhoto: null,
+        modal: false
+      };
+    default:
+      return state;
+  }
+};
+
+
 const useApplicationData = () => {
-  const [state, setState] = useState({
-    photos,
-    selectedPhoto: null, // Currently selected photo
-    favoritePhotos: [], // Array to store favorite photos
-    topics,
-    modal: false
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const onPhotoSelect = (photo) => {
-    setState((prevState) => ({
-      ...prevState,
-      selectedPhoto: photo,
-      modal: true
-    }));
+    dispatch({ type: 'SELECT_PHOTO', payload: photo });
   };
 
   const updateToFavPhotoIds = (photoId) => {
-    setState((prevState) => {
-      // handle repeated likes
-      const isPhotoInFavorites = prevState.favoritePhotos.includes(photoId);
-  
-      if (isPhotoInFavorites) {
-        // remove item from array if liked already (unlike)
-        return {
-          ...prevState,
-          favoritePhotos: prevState.favoritePhotos.filter(id => id !== photoId),
-        };
-      } else {
-        // if item not in array add item (like)
-        return {
-          ...prevState,
-          favoritePhotos: [...prevState.favoritePhotos, photoId],
-        };
-      }
-    });
+    dispatch({ type: 'TOGGLE_FAVORITE', payload: photoId });
   };
-
-    //    // handle repeated likes
-    //    if (favoritePhotos.includes(photoId)) {
-    //     // remove item from array if liked already (unlike)
-    //     setState(favoritePhotos.filter(id => id !== photoId));
-    //   } else {
-    //     // if item not in array add item (like)
-    //     setState([...favoritePhotos, photoId]); //likedPhotos.push(photoId)
-    //   }
-    // };
 
   const onClosePhotoDetailsModal = () => {
-    setState((prevState) => ({
-      ...prevState,
-      selectedPhoto: null,
-      modal:false
-    }));
+    dispatch({ type: 'CLOSE_MODAL' });
   };
-
 
   return {
     state,
     onPhotoSelect,
     updateToFavPhotoIds,
-    onClosePhotoDetailsModal,
+    onClosePhotoDetailsModal
   };
 };
 
 export default useApplicationData;
-
