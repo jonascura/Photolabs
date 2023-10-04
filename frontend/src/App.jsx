dc333fa6015f7ac69ae6fc493631f42e8563d53d
Jonas Cura <jonasm.cura@gmail.com.com>
2023-10-03 23:34:17 -0600

    moved logic for fetch api/photos and topics to useApplicationData.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,23 +9,6 @@ import useApplicationData from 'hooks/useApplicationData';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  const [photos, setPhotos] = useState([]);
-  const [topics, setTopics] = useState([]);
-
-  useEffect(() => {
-    // Fetch photos data
-    fetch('http://localhost:8001/api/photos')
-      .then(response => response.json())
-      .then(data => setPhotos(data))
-      .catch(error => console.error('Error fetching photos:', error));
-
-    // Fetch topics data
-    fetch('http://localhost:8001/api/topics')
-      .then(response => response.json())
-      .then(data => setTopics(data))
-      .catch(error => console.error('Error fetching topics:', error));
-  }, []); // Empty dependency array ensures useEffect runs once after initial render
-
   const {
     state,
     onPhotoSelect,
@@ -33,7 +16,7 @@ const App = () => {
     onClosePhotoDetailsModal,
   } = useApplicationData();
 
-  const { selectedPhoto, favoritePhotos, modal } = state;
+  const { photos, topics, selectedPhoto, favoritePhotos, modal } = state;
   // console.log("photos in app", photos)
 
   return (
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,11 +1,13 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 // import photos from 'mocks/photos';
 // import topics from 'mocks/topics';
 
 const initialState = {
   selectedPhoto: null,
   favoritePhotos: [],
-  modal:false
+  modal:false,
+  photos: [],
+  topics: []
 };
 
 const reducer = (state, action) => {
@@ -30,6 +32,16 @@ const reducer = (state, action) => {
         selectedPhoto: null,
         modal: false
       };
+    case 'SET_PHOTOS':
+      return {
+        ...state,
+        photos: action.payload
+      };
+    case 'SET_TOPICS':
+      return {
+        ...state,
+        topics: action.payload
+      };
     default:
       return state;
   }
@@ -38,6 +50,20 @@ const reducer = (state, action) => {
 
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    // Fetch photos data
+    fetch('http://localhost:8001/api/photos')
+      .then(response => response.json())
+      .then(data => dispatch({ type: 'SET_PHOTOS', payload: data }))
+      .catch(error => console.error('Error fetching photos:', error));
+
+    // Fetch topics data
+    fetch('http://localhost:8001/api/topics')
+      .then(response => response.json())
+      .then(data => dispatch({ type: 'SET_TOPICS', payload: data }))
+      .catch(error => console.error('Error fetching topics:', error));
+  }, []); // Empty dependency array ensures useEffect runs once after initial render
 
   const onPhotoSelect = (photo) => {
     dispatch({ type: 'SELECT_PHOTO', payload: photo });
